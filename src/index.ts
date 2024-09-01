75e683383026d88ec3b32715ea67ce13837effe2
Enchan <enchant0130@gmail.com>
2024-09-01 21:56:06 +0900

    [Update] specify location
    - コマンドラインオプションで対象の点を指定できるように

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs'
-import { PolygonFileSchema, readPoiygonFile as readPolygonFile } from 'polygon'
+import { Point, PolygonFileSchema, readPoiygonFile as readPolygonFile } from 'polygon'
 import { isPointInsideVectors, toVectors } from 'raycast'
 import * as yargs from 'yargs'
 
@@ -7,6 +7,16 @@ const args = yargs
     .option('input', {
         alias: 'i',
         description: 'Input file path',
+        demandOption: true
+    })
+    .option('x', {
+        description: 'target point x',
+        type: 'number',
+        demandOption: true
+    })
+    .option('y', {
+        description: 'target point y',
+        type: 'number',
         demandOption: true
     })
     .help()
@@ -20,5 +30,13 @@ const polygon = readPolygonFile(polygonFile)
 
 const vectors = toVectors(polygon)
 
-const result = isPointInsideVectors({ x: 0, y: 0 }, vectors)
-console.log(result)
+const toNumber = (n: unknown) => (isNaN(Number(n)) ? undefined : Number(n))
+
+const targetPoint: Point = { x: toNumber(args.x) ?? 0, y: toNumber(args.y) ?? 0 }
+
+const isInside = isPointInsideVectors(targetPoint, vectors)
+
+console.log(
+    `Polygon ${!isInside ? 'does not ' : ''}contains point (${targetPoint.x},${targetPoint.y}).`
+)
+process.exit(isInside ? 0 : 1)
